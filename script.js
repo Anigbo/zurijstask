9511b3de5bd07537387c0e3accc83100e4d6fe3b
Fredrick Anigbo <anigbofredrick@gmail.com>
2022-06-21 02:18:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let myData = {
+    name: "Fredrick",
+    height: 1.68,
+    country: "Nigeria"
+};
+
+document.getElementById("zuri").innerHTML = myData.name + ", " + myData.height + ", " + myData.country + ".";
+
